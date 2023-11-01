6c9b3c523fcbfc45c697f60e1043e5b5a9113adc
rishabh22204901 <rishabh22204901@gmail.com>
2023-11-01 09:20:54 +0530

    added comments

---
+++ 3.C Comments/comment.c
@@ -0,0 +1,17 @@
+//----------------------------------
+// Comments in C
+//----------------------------------
+
+// Comments can be used to explain code, and to make it more readable. It can also be used to prevent execution when testing alternative code.
+
+// Comments can be singled-lined or multi-lined.
+
+//----------------------
+//Single-line
+//----------------------
+
+/*
+------------------------
+Multi-line Comment
+-------------------------
+*/
