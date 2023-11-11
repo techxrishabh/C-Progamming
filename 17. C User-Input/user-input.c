93fcb044c16d3395c062d38f53e3ccf2058166af
rishabh22204901 <rishabh22204901@gmail.com>
2023-11-11 22:02:24 +0530

    added uuser-input

---
+++ 17. C User-Input/user-input.c
@@ -0,0 +1,16 @@
+// User Input
+// You have already learned that printf() is used to output values in C.
+
+// To get user input, you can use the scanf() function:
+
+#include <stdio.h>
+int main(){
+
+    // Create an integer variable that will store the number we get from the user
+    int myNum; 
+    // Ask the user to type a number
+    printf("Type a Number; \n");
+    // get and save the number the user types
+    scanf("%d", myNum);
+
+}
