0fbb2072aff3e81506d8a6254448f4d08c00d2df
rishabh22204901 <rishabh22204901@gmail.com>
2023-11-15 11:14:11 +0530

    added user input

--- 17. C User-Input/1.user-input.c
+++ 17. C User-Input/1.user-input.c
@@ -9,8 +9,14 @@ int main(){
     // Create an integer variable that will store the number we get from the user
     int myNum; 
     // Ask the user to type a number
-    printf("Type a Number; \n");
+    printf("Type a Number: \n");
     // get and save the number the user types
-    scanf("%d", myNum);
+    scanf("%d", &myNum);
+    // output the number the user typed
+    printf("Your number is: %d", myNum);
+
+    return 0;
+
+    // The scanf() function takes two arguments: the format specifier of the variable (%d in the example above) and the reference operator (&myNum), which stores the memory address of the variable.
 
 }
