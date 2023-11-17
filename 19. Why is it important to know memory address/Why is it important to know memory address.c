816973f60d9ddaade22aea8e95b48d058423186b
rishabh22204901 <rishabh22204901@gmail.com>
2023-11-17 21:19:15 +0530

    added importance of memory address

---
+++ 19. Why is it important to know memory address/Why is it important to know memory address.c
@@ -0,0 +1,5 @@
+// Why is it useful to know the memory address?
+
+// Pointers are important in C, because they allow us to manipulate the data in the computer's memory - this can reduce the code and improve the performance.
+
+// Pointers are one of the things that make C stand out from other programming languages, like Python and Java.
