704846d85700b8d82c8d56de0b406e5a0cef8107
rishabh22204901 <rishabh22204901@gmail.com>
2023-11-18 21:16:59 +0530

    added pointer

---
+++ 20. C Pointers/1. pointer.c
@@ -0,0 +1,19 @@
+// C Pointers
+
+// Creating Pointers
+// You learned from the previous chapter, that we can get the memory address of a variable with the reference operator &:
+
+#include <stdio.h>
+int main(){
+    int myAge = 47;
+
+    printf("%d", myAge);
+    printf("%p", myAge);  // Outputs the memory address of myAge (470000002F)
+}
+
+
+// A pointer is a variable that stores the memory address of another variable as its value.
+
+// A pointer variable points to a data type (like int) of the same type, and is created with the * operator.
+
+// The address of the variable you are working with is assigned to the pointer:
