4200535f6fd4d6fa336a24e5c2ce9402e814fd25
rishabh22204901 <rishabh22204901@gmail.com>
2023-11-20 21:26:46 +0530

    ADDED DEFERENCE

---
+++ 20. C Pointers/3. Dereference.c
@@ -0,0 +1,4 @@
+Dereference
+In the example above, we used the pointer variable to get the memory address of a variable (used together with the & reference operator).
+
+You can also get the value of the variable the pointer points to, by using the * operator (the dereference operator):
