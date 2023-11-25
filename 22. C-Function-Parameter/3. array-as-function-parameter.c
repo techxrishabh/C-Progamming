7aaca83ff475082f11f7eac74fcbb434967801f5
rishabh22204901 <rishabh22204901@gmail.com>
2023-11-25 19:15:11 +0530

    update

--- 22. C-Function-Parameter/3. array-as-function-parameter.c
+++ 22. C-Function-Parameter/3. array-as-function-parameter.c
@@ -16,3 +16,10 @@ int main(){
     return 0;
 
 }
+
+// Example Explained
+// The function (myFunction) takes an array as its parameter (int myNumbers[5]), and loops through the array elements with the for loop.
+
+// When the function is called inside main(), we pass along the myNumbers array, which outputs the array elements.
+
+// Note that when you call the function, you only need to use the name of the array when passing it as an argument myFunction(myNumbers). However, the full declaration of the array is needed in the function parameter (int myNumbers[5]).
